0e72ddfbbde93e9e67eda5a97538683019807895
Dr B <Brandon.borde@gmail.com>
2023-06-01 19:20:19 -0400

    Removed unnecessary functions and updated write to s3

--- get_data.py
+++ get_data.py
@@ -13,7 +13,6 @@ DS4A Data Engineering, Data Swan project
 
 
 import requests
-import urllib3
 import pandas as pd
 import boto3 as b3
 from botocore.config import Config
@@ -22,12 +21,12 @@ with open('./config.yaml', "r") as fl:
     config = yaml.safe_load(fl)
 
 
-s3_user = config.aws_user
-s3_key = config.aws_key
+s3_user = config['cloud_acct']['aws_user']
+s3_key = config['cloud_acct']['aws_key']
+s3_bucket_name = config['cloud_acct']['bucket_name']
+
 
 ######Getter functions########
-
-
 def get_cms_data_to_df(csv_links):
     # Takes a list of csv links and returns a dictionary of
     for item in csv_links:
@@ -35,22 +34,12 @@ def get_cms_data_to_df(csv_links):
     return cms_csv
 
 
-def write_to_s3(to_write, bucket_location):
-    # ds4a_s3 = b3.resource('s3', region='us-east-1', aws_access_key_id = s3_user, aws_secret_access_key=s3_key)
-    # bucket_object = ds4a_s3.Object(bucket_location, s3_key)
-    # bucket_object.put(to_write)
-
+def write_to_s3(to_write, bucket_location, write_key):
     # Using client object instead
-    s3_client = b3.client('s3', aws_access_key_id=s3_user,
+    s3_client = b3.client('s3',  aws_access_key_id=s3_user,
                           aws_secret_access_key=s3_key)
-    s3_client.upload_fileobj(to_write, bucket_location)
+    s3_client.put_object(Body=to_write, Bucket=bucket_location, Key=write_key)
     return print("Successfully wrote to S3 Bucket")
-
-
-# May not need this if we are recieving a one off pull
-def fetch_API_data(base_url, *params):
-    reviews_json = requests.get(base_url)
-    return reviews_json
 
 
 # Data Links
@@ -65,9 +54,10 @@ cms_links = {'gen_2020': 'https://download.cms.gov/openpayments/PGYR20_P012023/O
 cms_links_test = {
     'research_2020': 'https://download.cms.gov/openpayments/PGYR20_P012023/OP_DTL_RSRCH_PGYR2020_P01202023.csv'}
 
-for item, link in cms_links_test:
+# Gets the data and writes to S3 bucket
+for item, link in cms_links_test.items():
     response = requests.get(link)
     item_content = response.content
-    with open(f"{item}_.csv", "wb") as file:
-        file.write(item_content)
-        write_to_s3(file, 'ds4a_bucket')
+    print("successfully pulled data and generated content")
+    print("writing to S3")
+    write_to_s3(item_content, s3_bucket_name, f'cms_data/{item}_.csv')
