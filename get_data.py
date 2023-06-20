ac1b0174596ca902adef0942f250c5cbca4b2668
Dr B <Brandon.borde@gmail.com>
2023-06-20 13:37:06 -0400

    updated script to remove unnecessary columns

--- get_data.py
+++ get_data.py
@@ -21,21 +21,24 @@ s3_bucket_path = config['cloud_acct']['bucket_path']
 
 
 ####Define script constants#######
-# Data Links
-cms_links = {'gen_2020': ['general_payment', 'https://download.cms.gov/openpayments/PGYR20_P012023/OP_DTL_GNRL_PGYR2020_P01202023.csv'],
+# All Data Links
+cms_links = {'gen_2020': ['general_payment/', 'https://download.cms.gov/openpayments/PGYR20_P012023/OP_DTL_GNRL_PGYR2020_P01202023.csv'],
              'gen_2021': ['general_payment/', 'https://download.cms.gov/openpayments/PGYR21_P012023/OP_DTL_GNRL_PGYR2021_P01202023.csv'],
              'research_2020': ['research_payment/', 'https://download.cms.gov/openpayments/PGYR20_P012023/OP_DTL_RSRCH_PGYR2020_P01202023.csv'],
              'research_2021': ['research_payment/', 'https://download.cms.gov/openpayments/PGYR21_P012023/OP_DTL_RSRCH_PGYR2021_P01202023.csv'],
              'ownership_2020': ['ownership_payment/', 'https://download.cms.gov/openpayments/PGYR20_P012023/OP_DTL_OWNRSHP_PGYR2020_P01202023.csv'],
              'ownership_2021': ['ownership_payment/', 'https://download.cms.gov/openpayments/PGYR21_P012023/OP_DTL_OWNRSHP_PGYR2021_P01202023.csv']}
 
+# General Payments Data Only
+cms_gen_links_test = {'gen_2020': ['general_payment/', 'https://download.cms.gov/openpayments/PGYR20_P012023/OP_DTL_GNRL_PGYR2020_P01202023.csv'],
+                      'gen_2021': ['general_payment/', 'https://download.cms.gov/openpayments/PGYR21_P012023/OP_DTL_GNRL_PGYR2021_P01202023.csv']}
 
-cms_links_test = {
-    'research_2020': ['research_payment', 'https://download.cms.gov/openpayments/PGYR20_P012023/OP_DTL_RSRCH_PGYR2020_P01202023.csv']}
+# Single link to test
+cms_single_links_test = {'gen_2021': [
+    'general_payment/', 'https://download.cms.gov/openpayments/PGYR21_P012023/OP_DTL_GNRL_PGYR2021_P01202023.csv']}
+
 
 ######Helper functions########
-
-
 def write_to_s3(to_write, bucket_location, write_key, aws_user=s3_user, aws_secret=s3_key):
     # AWS with client object
     s3_client = b3.client('s3',  aws_access_key_id=aws_user,
@@ -54,10 +57,26 @@ def get_from_s3(to_get, bucket_location):
 
 
 # Gets the data and writes to S3 bucket
-for item, link in cms_links_test.items():
-    response = requests.get(link[1])
-    item_content = response.content
+# for item, link in cms_gen_links_test.items():
+#     response = requests.get(link[1])
+#     item_content = response.content
+#     print("successfully pulled data and generated content")
+#     print("writing to S3")
+#     write_to_s3(item_content, s3_bucket_name,
+#                 s3_bucket_path + link[0] + f'{item}_.csv')
+
+# Gets Data, and slices before writing to S3
+for item, link in cms_gen_links_test.items():
+    try:
+        del df
+    except:
+        pass
+    df = pd.read_csv(link[1], usecols=config['cms_columns'])
+    # df_sliced = df[config['cms_columns']]
     print("successfully pulled data and generated content")
     print("writing to S3")
-    write_to_s3(item_content, s3_bucket_name,
+    csv_buffer = io.BytesIO()
+    df.to_csv(csv_buffer)
+    write_to_s3(csv_buffer.getvalue(), s3_bucket_name,
                 s3_bucket_path + link[0] + f'{item}_.csv')
+    csv_buffer.close()
