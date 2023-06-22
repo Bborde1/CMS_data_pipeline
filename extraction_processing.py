ad12e055ff6ad926e66f956a8ca82aa2f1491d76
jori-kandra <jkandra@epi.org>
2023-06-22 14:53:48 -0400

    add code to generate unique geo_id

--- extraction_processing.py
+++ extraction_processing.py
@@ -10,6 +10,16 @@ import pandas as pd
 def update_general_payments(columns_list, general_df):
     # Get only columns that matter
     updated_df = general_df[[columns_list]]
+
+    # filter out observations with missing first name and last name
+    updated_df = general_df[general_df['Covered_Recipient_First_Name'].notnull()]
+    updated_df = general_df[general_df['Covered_Recipient_Last_Name'].notnull()]   
+
+    # concatenate addresses into one address
+    #note: corrects for any recipients at the same address, but in different suites
+    updated_df['Recipient_Primary_Business_Street_Address'] = updated_df['Recipient_Primary_Business_Street_Address_Line1'] + df['Recipient_Primary_Business_Street_Address_Line1']
+ 
+    
     return updated_df
 
 
